18d27de93fdfb88326edbc7b11d96345e9d8284e
=Arvind Kiwelekar <=awk@dbatu.ac.in>
2023-11-11 15:27:41 +0530

    errors removed

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,18 +1,18 @@
-    def add(x,y):
-...   return x+y
-... 
-... def substract(x, y):
-...     return x-y
-... 
-... def multiply(x, y):
-...     return x*y
-... 
-... def divide(x,y):
-...     return x/y
-... 
-... def max(x,y,z):
-...   m = x
-...   if(y>m):
-...     m=y
-...   if(z>m):
-...     m=z
+def add(x,y):
+    return x+y
+
+def substract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def max(x,y,z):
+ m = x
+ if(y>m):
+  m=y
+ if(z>m):
+  m=z
