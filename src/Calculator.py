09b26a127ccf4feacc7681d58a9a0b91f1481fda
=Arvind Kiwelekar <=awk@dbatu.ac.in>
2023-11-11 14:53:02 +0530

    src and tests folder added

---
+++ src/Calculator.py
@@ -0,0 +1,20 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def add(x,y):
+...   return x+y
+... 
+... def substract(x, y):
+...     return x-y
+... 
+... def multiply(x, y):
+...     return x*y
+... 
+... def divide(x,y):
+...     return x/y
+... 
+... def max(x,y,z):
+...   m = x
+...   if(y>m):
+...     m=y
+...   if(z>m):
+...     m=z
---
+++ tests/TestCalculator.py
@@ -0,0 +1,40 @@
+import unittest
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        '''Test case function for addition'''
+        result = add(4, 7)
+        expected = 11
+        self.assertEqual(result, expected)
+
+    def test_substract(self):
+        '''Test case function for subtraction'''
+
+        result = substract(10, 5)
+        expected = 5
+        self.assertEqual(result, expected)
+
+
+    def test_mul(self):
+        '''Test case function for multiplication'''
+        result = multiply(3, 7)
+        expected = 21
+        self.assertEqual(result, expected)
+
+    def test_div(self):
+        '''Test case function for division'''
+        result = divide(10, 2)
+        expected = 5
+        self.assertEqual(result, expected)
+
+    def test_max(self):
+        '''Test case function for max'''
+        result = max(10, 2,7)
+        expected = 10
+        self.assertEqual(result, expected)
+        result = max(2,10,7)
+        expected = 10
+        self.assertEqual(result, expected)
+        result = max(2,7,10)
+        expected = 10
+        self.assertEqual(result, expected)
